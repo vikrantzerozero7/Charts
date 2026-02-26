327571da7517f258769fdee20b65919792c11e69
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2026-02-26 13:31:49 +0530

    Update chart3.py

--- chart3.py
+++ chart3.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+def show():
+    st.subheader("Scatter Plot")
+
+    df = pd.DataFrame({
+        "x": np.random.randn(100),
+        "y": np.random.randn(100)
+    })
+
+    st.scatter_chart(df)
